8a731b3835d8f2772891f8bc9fe4637ca2b6064b
yjh1008 <exesea12@gmail.com>
2022-11-12 22:23:23 +0900

    feat : 단위테스트 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,6 +12,21 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    this.chkOverlap(numbers);
+    this.chkNumberValue(numbers);
+  }
+
+  chkOverlap(numbers) {
+    if([...new Set(numbers)].length !== 6) {
+      throw new Error("[ERROR] 중복된 값이 존재합니다.");
+    }
+  }
+  chkNumberValue(numbers) {
+    numbers.map((ball) => {
+      if(Math.sign(ball) !== 1){
+        throw new Error("[ERROR] 양수만 입력 가능합니다");
+      }
+    })
   }
 
   sortNumbers() {
